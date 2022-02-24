e48af9e6ed0ae5f2177aeaf67cb9319f3b7167ce
Panda <99999966@outlook.com>
2022-02-24 18:49:05 +0800

    2-1.JSX

--- react-learn/src/index.jsx
+++ react-learn/src/index.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const h1 = <h1>Hello World</h1>;
+const arr = new Array(30);
+arr.fill();
+const html = "<h1>test</h1>";
+const div = (
+  <div>
+    <p>{null}</p>
+    <ul>
+      {arr.map((item, i) => (
+        <li key={i}>{i}</li>
+      ))}
+    </ul>
+    <p
+      dangerouslySetInnerHTML={{
+        __html: html,
+      }}
+    ></p>
+  </div>
+);
 
-ReactDOM.render(h1, document.getElementById("root"));
+ReactDOM.render(div, document.getElementById("root"));
