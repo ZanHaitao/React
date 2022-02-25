5711cab60eb989c0f4ada237889e4994bcdcd38c
Panda <99999966@outlook.com>
2022-02-25 10:18:21 +0800

    2-2.[demo]图片切换

--- react-learn/src/index.jsx
+++ react-learn/src/index.jsx
@@ -1,23 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import img1 from "./assets/1.jpeg";
+import img2 from "./assets/2.jpeg";
+import img3 from "./assets/3.jpeg";
+import "./index.css";
 
-const arr = new Array(30);
-arr.fill();
-const html = "<h1>test</h1>";
-const div = (
-  <div>
-    <p>{null}</p>
-    <ul>
-      {arr.map((item, i) => (
-        <li key={i}>{i}</li>
-      ))}
-    </ul>
-    <p
-      dangerouslySetInnerHTML={{
-        __html: html,
-      }}
-    ></p>
-  </div>
-);
+const imgArr = [img1, img2, img3]; // 存放图片数组
+let index = 0;
+let timer;
+const container = document.getElementById("root");
 
-ReactDOM.render(div, document.getElementById("root"));
+/**
+ * 渲染
+ */
+function render() {
+  ReactDOM.render(<img src={imgArr[index]} alt="" />, container);
+}
+/**
+ * 启动定时器，切换图片
+ */
+function start() {
+  stop();
+  timer = setInterval(() => {
+    index = (index + 1) % 3;
+    render();
+  }, 2000);
+}
+/**
+ * 停止定时器
+ */
+function stop() {
+  clearInterval(timer);
+}
+container.onmouseenter = () => {
+  stop();
+};
+container.onmouseleave = () => {
+  start();
+};
+render();
+start();
