e0a0934892691b842e6042d839f096a1847b179e
Panda <99999966@outlook.com>
2022-02-27 16:26:18 +0800

    2-7.事件

--- react-learn/src/index.jsx
+++ react-learn/src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import BallList from './components/BallList'
+import TickControl from "./components/TickControl";
 
-ReactDOM.render(<BallList/>, document.getElementById("root"));
+ReactDOM.render(<TickControl />, document.getElementById("root"));
