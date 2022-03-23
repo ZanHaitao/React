65e65b6661be80ffd0df803dfc515781ee5d4182
Panda <99999966@outlook.com>
2022-03-23 16:06:33 +0800

    3-11.Portals

--- react-learn/src/components/Test.jsx
+++ react-learn/src/components/Test.jsx
@@ -1,35 +1,15 @@
 import React, { Component } from "react";
-import MouseListener from "./MouseListener";
+import ReactDOM from "react-dom";
 
+function Child() {
+  return ReactDOM.createPortal(<h1>Child</h1>, document.getElementById("test"));
+}
 export default class Test extends Component {
-  renderPanel = (state) => {
-    return (
-      <div
-        style={{
-          width: 100,
-          height: 100,
-          background: "red",
-          position: "absolute",
-          left: state.x - 50,
-          top: state.y - 50,
-        }}
-      ></div>
-    );
-  };
-
-  renderPoint = (state) => {
-    return (
-      <h1>
-        x:{state.x} y:{state.y}
-      </h1>
-    );
-  };
-
   render() {
     return (
       <>
-        <MouseListener render={this.renderPanel} />
-        <MouseListener render={this.renderPoint} />
+        <h1>Test</h1>
+        <Child />
       </>
     );
   }
