6f1fbce95e79ffc08cddb106a5d0d79b9c1c6c10
Panda <99999966@outlook.com>
2022-03-26 15:17:48 +0800

    4-4.useContextHook

--- react-learn/src/components/Student/getStudentData.js
+++
@@ -1,5 +0,0 @@
-const appkey = "15556677737_1585147447159";
-export default async function (page, size) {
-  const resp = await fetch(`/api/student/findByPage?appkey=${appkey}&page=${page}&size=${size}`)
-  return (await resp.json()).data;
-}
--- react-learn/src/components/Test.jsx
+++ react-learn/src/components/Test.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import StudentConatainer from './Student/StudentConatainer.jsx'
+import React, { useContext } from "react";
+const ctx = React.createContext();
+
+function TestContext(props) {
+  const value = useContext(ctx);
+  return <h1>上下文：{value}</h1>;
+}
 
 export default function Test() {
   return (
     <div>
-      <StudentConatainer />
+      <ctx.Provider value={"abc"}>
+        <TestContext />
+      </ctx.Provider>
     </div>
   );
 }
