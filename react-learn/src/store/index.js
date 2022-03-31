8dc172ac8ba2f783b0c7d2ec64b398832f4a9ebc
Panda <99999966@outlook.com>
2022-03-31 20:07:30 +0800

    6-8.Redux中间件

--- react-learn/src/store/index.js
+++ react-learn/src/store/index.js
@@ -1,7 +1,26 @@
-import { createStore, bindActionCreators, combineReducers } from '../redux'
+// import { createStore, bindActionCreators, combineReducers } from '../redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducer from './reducer';
 import { createAddUserAction } from './action/usersAction'
-const store = createStore(reducer);
+
+const logMiddleWare1 = store => next => action => {
+  console.log("中间件1");
+  console.log("获取旧数据", store.getState);
+  console.log("action", action);
+  next(action);
+  console.log("获取新数据", store.getState);
+}
+const logMiddleWare2 = store => next => action => {
+  console.log("中间件2");
+  console.log("获取旧数据", store.getState);
+  console.log("action", action);
+  next(action);
+  console.log("获取新数据", store.getState);
+}
+
+// const store = createStore(reducer, applyMiddleware(logMiddleWare1, logMiddleWare2));
+
+const store = applyMiddleware(logMiddleWare1, logMiddleWare2)(createStore)(reducer);
 
 console.log(store.getState());
 store.dispatch(createAddUserAction({
--- react-learn/src/store/reducer/loginUserReducer.js
+++ react-learn/src/store/reducer/loginUserReducer.js
@@ -4,7 +4,6 @@ const initialState = null;
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = initialState, { type, payload }) => {
-  console.log(type);
   switch (type) {
     case LOGINUSER:
       return payload
