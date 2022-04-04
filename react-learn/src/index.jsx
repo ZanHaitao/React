2365437125ebcc48171e17d8beca138994202239
Panda <99999966@outlook.com>
2022-04-04 17:55:08 +0800

    8-4.dva-loading

--- react-learn/src/index.jsx
+++ react-learn/src/index.jsx
@@ -2,10 +2,13 @@ import dva from "dva";
 import counterModel from "./models/counter";
 import configRouter from "./configRouter";
 import { createBrowserHistory } from "history";
+import createLoading from "dva-loading";
 
 const app = dva({
   history: createBrowserHistory(),
 });
+
+app.use(createLoading());
 
 app.model(counterModel);
 
